499ea0d4c477b5a6d03c08ce1642fc7e4a0ba6f8
Rowan Briggs <razrow@gmail.com>
2021-04-22 04:29:28 +0000

    Added a unit test

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -47,3 +47,13 @@ class TestValidator(unittest.TestCase):
 
         # Assert
         self.assertTrue(result)
+        
+    def test_it_will_reject_username_if_there_is_a_special_char(self):
+        #Assume
+        username = 'Rev@l'
+        
+        #Action
+        result = self.validator.username_is_valid(username)
+        
+        #Assert
+        self.assertFalse(result)
